d0daca2a40ff3b0c3661f3fef4f5c51dbf6971a2
duongdt <doanthanhduong11031990@gmail.com>
2021-05-11 16:30:58 +0700

    add start electron project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Electron_AutoDeeplinkTest
+
+npm start
+
+https://github.com/xiaowuming/adb-tools
---
+++ electron-quick-start/preload.js
@@ -0,0 +1,100 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+function rootPath() {
+  return __dirname;
+} 
+
+var AndroidDevice = function (adb, id, type) {
+  this.adb = adb;
+  this.id = id;
+  this.type = type;
+};
+
+
+function parseDevices(data) {
+  var lines = data.toString().split('\n');
+  var devices = [];
+
+  for (var i=0; i<lines.length; i++) {
+      var o = lines[i].split('\t');
+
+      if (o.length == 2) {
+          devices.push(new AndroidDevice(o[0], o[1]));
+      }
+  }
+
+  return devices;
+}
+
+function callAdb(adbPath, options, next) {
+  var exec = require('child_process').spawn;
+
+  var a = adbPath + 'adb';
+
+  var cmd = options.cmd;
+
+  if (typeof options.deviceID === 'string') {
+      cmd.push('-s');
+      cmd.push(options.deviceID);
+  }
+
+  var ls = exec(a, cmd);
+  var useNext = false;
+  ls.stdout.on('data', function (data) {
+      useNext = true;
+      next && next(data.toString());
+  });
+
+  ls.stderr.on('data', function (data) {
+      //console.log(data.toString());
+  });
+
+  ls.on('exit', function () {
+      if (typeof next === 'function') {
+          if (useNext === false) {
+              setTimeout(function () {
+                  next();
+              }, 200);
+          }
+      }
+  });
+  return ls;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+
+  const path = require('path');
+  const rootPathApp = rootPath();
+  const configPathApp = path.resolve(`${rootPathApp}/configs`);
+  
+  const fs = require('fs')
+  const files = fs.readdirSync(configPathApp, {
+    withFileTypes: true,
+  }).filter(fileEnt => fileEnt.isFile())
+  .map(fileEnt => fileEnt.name);
+
+  var configCombobox = document.getElementById("configCombobox");
+
+  var i;
+  for (i = 0; i < files.length; i++) {
+    var option = document.createElement("option");
+    option.text = files[i];
+    configCombobox.add(option); 
+  }
+
+  const adbPath = rootPathApp + "\\adb\\"
+  callAdb(adbPath, {
+        cmd: ['devices']
+    }, function(result) {
+      console.log(parseDevices(result))
+    });
+})
